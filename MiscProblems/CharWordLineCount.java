767dd1646a57b6e11037b4fd29b8ce3d4628afa2
Karan Warrier <kawarrier@yahoo.com>
2020-05-07 00:23:32 -0700

    solved the charWordLine count problem

--- MiscProblems/CharWordLineCount.java
+++ MiscProblems/CharWordLineCount.java
@@ -16,56 +16,65 @@ class CharWordLineCount {
   
     // split charSeen into wordSeen and lineSeen
   
+    public static boolean noCharactersBetween(String str, int start, int end) {
+	if (start < 0) return false;
+	for (int i = start+1; i < end; i++) {
+	    if (Character.isLetter(str.charAt(i))) return false;
+	}
+	return true;
+    }
+
     public static String getCounts(String str){ 
 	int charCount = 0;
 	int wordCount = 0;
 	int lineCount = 0;
+
+	int tempWords = 0;
+	int tempLines = 0;
+
 	char prev = ' ';
-	boolean charSeen = false;
-	boolean wordSeen = true;
-	boolean lineSeen = true;
+	int prevCh = -1;
+
+	boolean lineSeen = false;
+	boolean spaceSeen = false;
 	for (int i = 0; i < str.length(); i++) {
 	    char ch = str.charAt(i);
-      
-	    if (ch != ' ' && ch != '\n') {
-		charCount++;
-		//charSeen = true;
-		if (wordSeen) {
+	    if (Character.isLetter(ch)) {
+		if (spaceSeen && tempLines == 0) {
 		    wordCount++;
-		    wordSeen = false;
+		    spaceSeen = false;
 		}
-		if (lineSeen) {
-		    System.out.println(wordSeen);
-		    lineCount++;
-		    lineSeen = false;
-		    if (wordSeen) {
-			wordCount++;
-			wordSeen = false;
-		    }
+                if (tempLines > 0) {
+		    lineCount += tempLines;
+		    if (noCharactersBetween(str, prevCh, i)) wordCount++;
+		    tempLines = 0;
 		}
+		charCount++;
+		prevCh = i;
 	    } else if (ch == ' ') {
-		//charSeen = false;
-		//wordCount++;
-		wordSeen = true;
+		spaceSeen = true;
 	    } else if (ch == '\n') {
-		//lineCount ++;
-		lineSeen = true;
+		tempLines++;
 	    }
 	    prev = ch;
 	}
-	return "("+charCount+" "+wordCount+" "+lineCount+")";
+	if (charCount > 0) {
+	    lineCount++;
+	}
+	if (Character.isLetter(prev)) wordCount++;
+	return "("+charCount+", "+wordCount+", "+lineCount+")";
     }
   
     public static void main(String[] args) {
-	System.out.println(getCounts("a"));
-	System.out.println(getCounts("a b\nc"));
-	System.out.println(getCounts("\na"));
-	System.out.println(getCounts("abc def"));
-	/*System.out.println(getCounts("\n a"));
-    System.out.println(getCounts(" ab "));
-    System.out.println(getCounts(" ab  "));
-    System.out.println(getCounts("\n"));
-    System.out.println(getCounts("\n\na"));*/
+       	System.out.println(getCounts("a") +" should be (1, 1, 1)");
+	System.out.println(getCounts("a b\nc") +" should be (3, 3, 2)");
+	System.out.println(getCounts("\na") +" should be (1, 1, 2)");
+	System.out.println(getCounts("abc def")+" should be (6, 2, 1)");
+	System.out.println(getCounts("\n a")+" should be (1, 1, 2)");
+	System.out.println(getCounts(" ab ")+" should be (2, 1, 1)");
+	System.out.println(getCounts(" ab  ")+" should be (2, 1, 1)");
+	System.out.println(getCounts("\n")+" should be (0, 0 ,0)");
+	System.out.println(getCounts("\n\na")+" should be (1, 1, 3)");
     }
 }
 
