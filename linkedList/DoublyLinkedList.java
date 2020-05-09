2cd2dae0f4336c6f0b1bafedefe42084d2f5bdd7
Karan Warrier <kawarrier@yahoo.com>
2020-05-09 15:23:39 -0700

    doubly linked list

--- linkedList/DoublyLinkedList.java
+++ linkedList/DoublyLinkedList.java
@@ -1,103 +1,133 @@
 public class DoublyLinkedList {
 
+    public Node head;
+    public Node tail;
+
     public DoublyLinkedList() {
 
     }
 
-
-    static class DoublyLinkedList {
-	public Node head;
-	public Node tail;
-
-	public void setHead(Node node) {
-	    // Write your code here.
-	    node.next = this.head.next;
-	    this.head = node;
+    //null <- 1 -> <- 2 -> <- 3 -> <- 4 -> <- 5 -> null
+    
+    public void setHead(Node node) {
+	if (head == null) {
+	    head = node;
+	    tail = node;
+	} else {
+	    head.prev = node;
+	    node.next = head;
+	    head = node;
 	}
-
-	public void setTail(Node node) {
-	    // Write your code here.
-	    node.prev = this.tail.prev;
-	    this.tail = node;
+    }
+    
+    public void setTail(Node node) {
+	if (tail == null) {
+	    tail = node;
+	    head = node;
+	} else {
+	    tail.next = node;
+	    node.prev = tail;
+	    tail = node;
 	}
-
-	public void insertBefore(Node node, Node nodeToInsert) {
-	    if (node == this.head) {
-		setHead(nodeToInsert);
-	    } else {
-		Node prev = node.prev;
-		node.prev = nodeToInsert;
-		prev.next = nodeToInsert;
-		nodeToInsert.prev = prev;
-		nodeToInsert.next = node;
+    }
+    
+    public void insertBefore(Node node, Node nodeToInsert) {
+	if (node == head) {
+	    setHead(nodeToInsert);
+	} else {
+	    Node prev = node.prev;
+	    prev.next = nodeToInsert;
+	    node.prev = nodeToInsert;
+	    nodeToInsert.prev = prev;
+	    nodeToInsert.next = node;
+	}
+    }
+    
+    public void insertAfter(Node node, Node nodeToInsert) {
+	if (node == tail) {
+	    setTail(nodeToInsert);
+	} else {
+	    Node next = node.next;
+	    next.prev = nodeToInsert;
+	    node.next = nodeToInsert;
+	    nodeToInsert.prev = node;
+	    nodeToInsert.next = next;
+	}
+    }
+    
+    public void insertAtPosition(int position, Node nodeToInsert) {
+	Node ptr = head;
+	while (ptr != null && position > 0) {
+	    position--;
+	    ptr = ptr.next;
+	}
+	insertAfter(ptr, nodeToInsert);
+    }
+    
+    public void removeNodesWithValue(int value) {
+	Node ptr = head;
+	while (ptr != null) {
+	    if (ptr.value == value) {
+		remove(ptr);
 	    }
 	}
-
-	public void insertAfter(Node node, Node nodeToInsert) {
-	    if (node == this.tail) {
-		setTail(nodeToInsert);
-	    } else {
-		Node next = node.next;
-		node.next = nodeToInsert;
-		next.prev = nodeToInsert;
-		nodeToInsert.prev = node;
-		nodeToInsert.next = next;
-	    }  
+    }
+    
+    public void remove(Node node) {
+	if (node == head) {
+	    head = head.next;
+	    head.prev = null;
+	} else if (node == tail) {
+	    tail = tail.prev;
+	    tail.next = null;
+	} else {
+	    Node prev = node.prev;
+	    Node next=  node.next;
+	    prev.next = next;
+	    next.prev = prev;
 	}
-
-	public void insertAtPosition(int position, Node nodeToInsert) {
-	    // Write your code here.
-	    Node curr = this.head;
-	    while (position > 0) {
-		curr = curr.next;
-	    }
-	    insertAfter(curr, nodeToInsert);
+    }
+    
+    public boolean containsNodeWithValue(int value) {
+	Node copy = head;
+	while (copy != null) {
+	    if (copy.value == value) return true;
+	    copy = copy.next;
 	}
+	return false;
+    }
 
-	public void removeNodesWithValue(int value) {
-	    Node curr = this.head;
-	    while (curr != null) {
-		Node removal = curr;
-		if (curr.value == value) {
-		    remove(removal);
-		}
-		curr = curr.next;
-	    }
-	}
-
-	public void remove(Node node) {
-	    if (node == this.head) {
-		this.head = this.head.next;
-	    } if (node == this.tail) {
-		this.tail = this.tail.prev;
-	    }
-	    if (node.prev != null) {
-		node.prev.next = node.next;
-	    }
-	    if (node.next != null) {
-		node.next.prev = node.prev;
-	    }
-	    node.prev = null;
-	    node.next = null;
-	}
-
-	public boolean containsNodeWithValue(int value) {
-	    Node curr = this.head;
-	    while (curr != null && curr.value != value) {
-		curr = curr.next;
-	    }
-	    return curr != null;
+    public void printLst() {
+	Node ptr = head;
+	while (ptr != null) {
+	    System.out.println(ptr.value);
+	    ptr = ptr.next;
 	}
     }
 
-    // Do not edit the class below.
-    static class Node {
-	public int value;
-	public Node prev;
-	public Node next;
+    public static void main(String[] args) {
+	DoublyLinkedList lst = new DoublyLinkedList();
+	Node first = new Node(1);
+	Node second = new Node(2);
+	Node third = new Node(3);
+	Node fourth = new Node(4);
+	Node fifth = new Node(5);
+	lst.setHead(first);
+	lst.insertAfter(first, second);
+	lst.insertAfter(second, third);
+	lst.insertAfter(third, fourth);
+	lst.insertAfter(fourth, fifth);
+	lst.remove(third);
+	lst.printLst();
+    }
+}
 
-	public Node(int value) {
-	    this.value = value;
-	}
+class Node {
+    public int value;
+    public Node prev;
+    public Node next;
+    
+    public Node(int value) {
+	this.value = value;
     }
 }
